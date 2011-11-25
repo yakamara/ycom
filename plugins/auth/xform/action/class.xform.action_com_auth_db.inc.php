7b53afa7ce47b6ee76116bb910ed8163097ae67d
ovo2 <info@sdsoft.it>
2011-11-25 14:35:21 +0100

    Update plugins/auth/xform/action/class.xform.action_com_auth_db.inc.php

--- plugins/auth/xform/action/class.xform.action_com_auth_db.inc.php
+++ plugins/auth/xform/action/class.xform.action_com_auth_db.inc.php
@@ -53,3 +53,32 @@ class rex_xform_action_com_auth_db extends rex_xform_action_abstract
 }
 
 ?>
+
+<?php
+class rex_xform_action_com_login_after_reg extends rex_xform_action_abstract
+{
+	function execute()
+	{
+		global $REX;
+
+		#sddebug( 'rex_xform_action_com_login_after_reg' );
+		
+		$auth_name	= $REX['ADDON']['community']['plugin_auth']['request']['name'];
+		$auth_psw	= $REX['ADDON']['community']['plugin_auth']['request']['psw'];
+		
+		if ( isset($this->params["value_pool"]["sql"]["login"]) && isset($this->params["value_pool"]["sql"]["password"]) )
+		{
+			$login 		= $this->params["value_pool"]["sql"]["login"];
+			$password 	= $this->params["value_pool"]["sql"]["password"];
+	
+			header( 'Location:'.rex_getUrl( $this->getElement(1) ,$REX['CUR_CLANG'], array($auth_name=>$login,$auth_psw=>$password), '&' ) );
+		}
+
+	}
+
+	function getDescription()
+	{
+		return "<strong>action|com_login_after_reg|article_id</strong><br><i>Benutzer wird nach der Registrierung zum Artikel mit article_id weitergeleitet und automatisch angemeldet.</i><br>Beispiel:action|com_login_after_reg|55";
+	}
+}
+?>
