45341a42256f5108eeed831142d2e3724fcaf074
jdlx <github@rexdev.de>
2012-06-24 14:09:01 +0300

    fix: checkArticlePerm() -> checkPerm()

--- plugins/auth/classes/class.rex_com_navigation.inc.php
+++ plugins/auth/classes/class.rex_com_navigation.inc.php
@@ -222,7 +222,7 @@ class rex_com_navigation extends rex_navigation
   
   function _checkPerm($nav,$level)
   {
-    return rex_com_auth::checkArticlePerm($nav);
+    return rex_com_auth::checkPerm($nav);
   }
   
   
