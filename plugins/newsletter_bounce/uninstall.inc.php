871b91399a54f2cdd1acf8443ead4ce57ae58907
Jan Kristinus <jan.kristinus@yakamara.de>
2012-12-05 23:09:53 +0100

    Newsletter Bounce Counter (un)install

--- plugins/newsletter_bounce/uninstall.inc.php
+++ plugins/newsletter_bounce/uninstall.inc.php
@@ -1,7 +1,9 @@
 <?php
 
+/*
 $sql = new rex_sql();
 $sql->setQuery('ALTER TABLE `' . $REX['TABLE_PREFIX'] . 'com_user` DROP `bounce_counter`');
+*/
 
 $REX['ADDON']['install']['newsletter_bounce'] = false;
 
