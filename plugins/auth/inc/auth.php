1108946487c6a957e7e8ef49566f7d56ea181afe
Jan Kristinus <jan.kristinus@yakamara.de>
2012-12-05 23:10:10 +0100

    Formatierungen angepasst

--- plugins/auth/inc/auth.php
+++ plugins/auth/inc/auth.php
@@ -21,20 +21,19 @@ $redirect = '';
 $login_status = rex_com_auth::login($login_name, $login_psw, $login_stay, $logout);
 
 ## set redirect
-if($login_status == 2)
-{
-  if($referer)
+if ($login_status == 2) {
+  if ($referer) {
     $redirect = urldecode($referer);
-  else 
+  } else { 
     $redirect = rex_getUrl($REX['ADDON']['community']['plugin_auth']['article_login_ok']);
+  }
 }
 
 /*
  * Checking page permissions
  */
-if($article = OOArticle::getArticleById($REX["ARTICLE_ID"]))
-  if(!rex_com_auth::checkperm($article) && !$redirect  && $REX['ADDON']['community']['plugin_auth']['article_withoutperm'] != $REX['ARTICLE_ID'])
-  {
+if ($article = OOArticle::getArticleById($REX["ARTICLE_ID"])) {
+  if(!rex_com_auth::checkperm($article) && !$redirect  && $REX['ADDON']['community']['plugin_auth']['article_withoutperm'] != $REX['ARTICLE_ID']) {
     $params = null;
     
     ## Adding referer only if target is not login_ok Article
@@ -43,12 +42,12 @@ if($article = OOArticle::getArticleById($REX["ARTICLE_ID"]))
     
     $redirect = rex_getUrl($REX['ADDON']['community']['plugin_auth']['article_withoutperm'], '', $params, '&');
   }
+}
   
 /*
  * Handle redirects if required
  */
-if($redirect)
-{
+if($redirect) {
   ob_end_clean();
   header('Location:'.$redirect); 
   exit;
