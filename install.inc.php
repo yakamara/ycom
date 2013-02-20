f7ef8424980e428a76f01f51cc459b10c3b00210
Jan Kristinus <jan.kristinus@yakamara.de>
2013-02-20 14:15:45 +0100

    install geht nur noch bei rex 4.5 oder höher

--- install.inc.php
+++ install.inc.php
@@ -1,52 +1,52 @@
 <?php
 
 /**
- * Community Install 
+ * Community Install
  * @author jan.kristinus[at]redaxo[dot]de Jan Kristinus
  * @author <a href="http://www.yakamara.de">www.yakamara.de</a>
  */
 
-$addonname = "community";
+$addonname = 'community';
 
 $I18N->appendFile($REX['INCLUDE_PATH'] . '/addons/community/lang');
 
-if (OOAddon::isAvailable('phpmailer') != 1 || version_compare(OOAddon::getVersion('phpmailer'),"2.8",'<')) 
-{
-	$REX['ADDON']['install']['community'] = 0;
-	$REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_phpmailer_version_problem','2.8');
 
-}elseif(OOAddon::isAvailable('xform') != 1 || version_compare(OOAddon::getVersion('xform'), "4.5", '<')) 
-{
-	$REX['ADDON']['install']['community'] = 0;
-	$REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_xform_version_problem','4.5');
+if ($REX['VERSION'] != '4' || $REX['SUBVERSION'] < '5') {
+  $REX['ADDON']['install']['community'] = 0;
+  $REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_redaxo_version_problem', '4.5');
 
-}elseif(OOPlugin::isAvailable('xform','manager') != 1 || version_compare(OOPlugin::getVersion("xform", "manager"),"4.5",'<')) 
-{
-	$REX['ADDON']['install']['community'] = 0;
-	$REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_xform_manager_version_problem','4.5');
+} elseif (OOAddon::isAvailable('phpmailer') != 1 || version_compare(OOAddon::getVersion('phpmailer'), '2.8', '<')) {
+  $REX['ADDON']['install']['community'] = 0;
+  $REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_phpmailer_version_problem', '2.8');
 
-}elseif(OOPlugin::isAvailable('xform','email') != 1 || version_compare(OOPlugin::getVersion("xform", "email"),"4.5",'<')) 
-{
-	$REX['ADDON']['install']['community'] = 0;
-	$REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_xform_email_version_problem','4.5');
+} elseif (OOAddon::isAvailable('xform') != 1 || version_compare(OOAddon::getVersion('xform'), '4.5', '<')) {
+  $REX['ADDON']['install']['community'] = 0;
+  $REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_xform_version_problem', '4.5');
 
-}else
-{
+} elseif (OOPlugin::isAvailable('xform', 'manager') != 1 || version_compare(OOPlugin::getVersion('xform', 'manager'), '4.5', '<')) {
+  $REX['ADDON']['install']['community'] = 0;
+  $REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_xform_manager_version_problem', '4.5');
 
-  $msg = "";
+} elseif (OOPlugin::isAvailable('xform', 'email') != 1 || version_compare(OOPlugin::getVersion('xform', 'email'), '4.5', '<')) {
+  $REX['ADDON']['install']['community'] = 0;
+  $REX['ADDON']['installmsg']['community'] = $I18N->msg('community_install_xform_email_version_problem', '4.5');
+
+} else {
+
+  $msg = '';
 
   /*
 
   // AUTOINSTALL THESE PLUGINS
   $autoinstall = array('auth','group');
-  
+
   // GET ALL ADDONS & PLUGINS
   $all_addons = rex_read_addons_folder();
   $all_plugins = array();
   foreach($all_addons as $_addon) {
     $all_plugins[$_addon] = rex_read_plugins_folder($_addon);
   }
-  
+
   // DO AUTOINSTALL
   $pluginManager = new rex_pluginManager($all_plugins, $addonname);
   foreach($autoinstall as $pluginname) {
@@ -55,42 +55,40 @@ if (OOAddon::isAvailable('phpmailer') != 1 || version_compare(OOAddon::getVersio
     {
       $msg = $instErr;
     }
-  
+
     // ACTIVATE PLUGIN
     if ($msg == '' && ($actErr = $pluginManager->activate($pluginname)) !== true)
     {
       $msg = $actErr;
     }
-  
+
     if($msg != '')
     {
       break;
     }
   }
-  
+
   */
 
-  if ($msg != '')
-  {
+  if ($msg != '') {
     $REX['ADDON']['installmsg'][$addonname] = $msg;
-  
-  }else
-  {
 
-  	$REX['ADDON']['install']['community'] = 1;
-  
-  	function rex_com_install() 
-  	{
-  		$r = new rex_xform_manager;
-  		$r->generateAll();
-  	}
-  	rex_register_extension('OUTPUT_FILTER', 'rex_com_install');
-  
+  } else {
+
+    $REX['ADDON']['install']['community'] = 1;
+
+    function rex_com_install()
+    {
+      $r = new rex_xform_manager;
+      $r->generateAll();
+    }
+    rex_register_extension('OUTPUT_FILTER', 'rex_com_install');
+
     $c_file = $REX['INCLUDE_PATH'] . '/addons/community/install/rex_4.5_community';
     $ie_file = $REX['INCLUDE_PATH'] . '/addons/import_export/backup/rex_4.5_community';
-  
-    copy ( $c_file.'.sql' , $ie_file.'.sql' );
-    copy ( $c_file.'.tar.gz' , $ie_file.'.tar.gz' );
+
+    copy( $c_file . '.sql' , $ie_file . '.sql' );
+    copy( $c_file . '.tar.gz' , $ie_file . '.tar.gz' );
 
   }
 
