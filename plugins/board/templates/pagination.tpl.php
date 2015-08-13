84ad7ec59d447acbf65cff30186554c756850725
Gregor Harlan <gregor.harlan@yakamara.de>
2015-08-13 16:04:27 +0200

    Forum: Pagination nur anzeigen wenn notwendig

--- plugins/board/templates/pagination.tpl.php
+++ plugins/board/templates/pagination.tpl.php
@@ -4,8 +4,12 @@
     $pager = $this->getPager();
 ?>
 
+<?php if ($pager->getRowCount() > $pager->getRowsPerPage()): ?>
+
 <div class="pagination">
     <?php for ($page = $pager->getFirstPage(); $page <= $pager->getLastPage(); ++$page): ?>
-        <a<?= $pager->isActivePage($page) ? ' class="active"' : '' ?> href="<?= $this->getCurrentUrl(array('start' => $pager->getCursor($page))) ?>"><?= $page + 1 ?></a>
+        <a<?= $pager->isActivePage($page) ? ' class="pagination__item active-page"' : ' class="pagination__item"' ?> href="<?= $this->getCurrentUrl(array('start' => $pager->getCursor($page))) ?>"><?= $page + 1 ?></a>
     <?php endfor ?>
 </div>
+
+<?php endif ?>
