3f1ffeaf562d7e6b55d2c9aca5ca3b5736e011fc
Jan Kristinus <jan.kristinus@yakamara.de>
2015-09-08 13:59:50 +0200

    auth: xform auth login angepasst. Werte escaped

--- plugins/auth/xform/validate/class.xform.validate_com_auth_login.inc.php
+++ plugins/auth/xform/validate/class.xform.validate_com_auth_login.inc.php
@@ -6,12 +6,11 @@ class rex_xform_validate_com_auth_login extends rex_xform_validate_abstract
 	function enterObject()
 	{
 		global $REX;
-		
-		$error = TRUE;
+
 		$vars = array();
-		
+		$sql = rex_sql::factory();
+
 		$e = explode(",",$this->getElement(2));
-		$s = array();
 		foreach ($e as $v) {
 			$w = explode("=",$v);
 			$label = $w[0];
@@ -19,33 +18,34 @@ class rex_xform_validate_com_auth_login extends rex_xform_validate_abstract
 			$vars[$label] = $value;
 		}
 
-    $query_extras = "";
-    if ($this->getElement(3) != "") {
-      $query_extras = ' and ('.$this->getElement(3).')';
-    }
-
-    rex_com_auth::loginWithParams($vars,$query_extras);
-
-    if (!rex_com_auth::getUser()) {
-      $this->params["warning"][] = 1;
-      $this->params["warning_messages"][] = rex_translate($this->getElement(4));
-      rex_com_auth::clearUserSession();
-      
-    } else {
-      // Load fields for eMail or DB
-      $fields = $this->getElement(5);
-      if ($fields != "") {
-        $fields = explode(",",$fields);
-        foreach($fields as $field) {
-          $this->params["value_pool"]["email"][$field] = $REX["COM_USER"]->getValue($field);
-          if ($this->getElement(6) != "no_db") 
-            $this->params["value_pool"]["sql"][$field] = $REX["COM_USER"]->getValue($field);
+        $query_extras = "";
+        if ($this->getElement(3) != "") {
+            $query_extras = ' and ('.$this->getElement(3).')';
         }
-      }
+
+        rex_com_auth::loginWithParams($vars,$query_extras);
+
+        if (!rex_com_auth::getUser()) {
+            $this->params["warning"][] = 1;
+            $this->params["warning_messages"][] = rex_translate($this->getElement(4));
+            rex_com_auth::clearUserSession();
+
+        } else {
+            // Load fields for eMail or DB
+            $fields = $this->getElement(5);
+            if ($fields != "") {
+                $fields = explode(",",$fields);
+                foreach($fields as $field) {
+                    $this->params["value_pool"]["email"][$field] = $REX["COM_USER"]->getValue($field);
+                    if ($this->getElement(6) != "no_db") {
+                        $this->params["value_pool"]["sql"][$field] = $sql->escape($REX["COM_USER"]->getValue($field));
+                    }
+                }
+            }
   
-    }
+        }
     
-  	return;
+  	    return;
 	}
 	
 	function getDescription()
