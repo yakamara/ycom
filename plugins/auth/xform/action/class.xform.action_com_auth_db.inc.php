f189df2a4b4a4f46137c2d4c072e0b0fb8ee2a7e
Jan Kristinus <jan.kristinus@yakamara.de>
2015-09-08 14:00:02 +0200

    auth: action auth db umformatiert

--- plugins/auth/xform/action/class.xform.action_com_auth_db.inc.php
+++ plugins/auth/xform/action/class.xform.action_com_auth_db.inc.php
@@ -7,38 +7,41 @@ class rex_xform_action_com_auth_db extends rex_xform_action_abstract
 	{
 		global $REX;
 	
-		if(!isset($REX["COM_USER"])) {
+		if (!isset($REX["COM_USER"])) {
 
 			echo "error - access denied - user not logged in";
 			
-		}else {
+		} else {
 
-			if($this->getElement(2) == "logout") {
-				$REX['COM_USER']->setLogout(true);
-				echo $REX['COM_USER']->checkLogin();
-				unset($REX["COM_USER"]);
-				unset($_COOKIE['comrex_auth']);
+			switch($this->getElement(2)){
 
-			}elseif($this->getElement(2) == "delete") {
+				case("logout"):
+					$REX['COM_USER']->setLogout(true);
+					echo $REX['COM_USER']->checkLogin();
+					unset($REX["COM_USER"]);
+					unset($_COOKIE['comrex_auth']);
+					break;
 
-				rex_com_auth::deleteUser($REX["COM_USER"]->getValue("id"));
-				rex_com_auth::clearUserSession();
-			
-			}else {
+				case("delete"):
+					rex_com_auth::deleteUser($REX["COM_USER"]->getValue("id"));
+					rex_com_auth::clearUserSession();
+					break;
 
-				$sql = rex_sql::factory();
-				if ($this->params["debug"]) {
-					$sql->debugsql = TRUE;
-		    	}
-		    
-		    	$sql->setTable("rex_com_user");
-				foreach($this->params["value_pool"]["sql"] as $key => $value) {
-					$sql->setValue($key, $value);
-				}
-	
-				$sql->setWhere('id='.$REX["COM_USER"]->getValue("id").'');
-				$sql->update();
+				case("update"):
+				default:
+					$sql = rex_sql::factory();
+					if ($this->params["debug"]) {
+						$sql->debugsql = TRUE;
+					}
 
+					$sql->setTable("rex_com_user");
+					foreach($this->params["value_pool"]["sql"] as $key => $value) {
+						$sql->setValue($key, $value);
+					}
+
+					$sql->setWhere('id='.$REX["COM_USER"]->getValue("id").'');
+					$sql->update();
+					break;
 			}
 
 		}
