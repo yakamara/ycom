819121c61a578be9a4bb74d1ec5a81a97f8a5e8c
Jan Kristinus <jan.kristinus@yakamara.de>
2016-04-08 23:50:34 +0200

    warning entfernt: pages werden nun nur im backend eingefügt

--- boot.php
+++ boot.php
@@ -2,20 +2,22 @@
 
 rex_ycom::addTable(rex_ycom_user::getTable());
 
-rex_extension::register('PACKAGES_INCLUDED', function ($params) {
+if (rex::isBackend()) {
+    rex_extension::register('PACKAGES_INCLUDED', function ($params) {
 
-    $plugin = rex_plugin::get('yform','manager');
+        $plugin = rex_plugin::get('yform','manager');
 
-    if ($plugin) {
-        $pages = $plugin->getProperty('pages');
-        $ycom_tables = rex_ycom::getTables();
+        if ($plugin) {
+            $pages = $plugin->getProperty('pages');
+            $ycom_tables = rex_ycom::getTables();
 
-        foreach ($pages as $page) {
-            if (in_array($page->getKey(), $ycom_tables)) {
-                $page->setBlock('ycom');
+            foreach ($pages as $page) {
+                if (in_array($page->getKey(), $ycom_tables)) {
+                    $page->setBlock('ycom');
+                }
             }
+
         }
 
-    }
-
-});
+    });
+}
