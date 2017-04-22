3d2466018ada8b3c0f15eb324a605542a17153be
Jan Kristinus <jan.kristinus@yakamara.de>
2017-04-22 15:01:38 +0200

    yform: user init ergänzt um main_table und main_id zu setzen

---
+++ lib/yform/value/ycom_user_init.php
@@ -0,0 +1,22 @@
+<?php
+
+class rex_yform_value_ycom_user_init extends rex_yform_value_abstract
+{
+    public function enterObject()
+    {
+        if (rex_ycom_auth::getUser()) {
+            $this->params['main_table'] = 'rex_ycom_user';
+            $this->params['main_id'] = rex_ycom_auth::getUser()->getId();
+
+        } else {
+
+            $this->params['warning'][$this->getId()] = $this->getElement(3);
+
+        }
+    }
+
+    public function getDescription()
+    {
+        return 'ycom_user_init -> Beispiel: ycom_user_init|[label]|[name]|error_msg';
+    }
+}
