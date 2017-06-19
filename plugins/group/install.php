e092f155c50f43ccce34b4b1bcafa5dc9c194432
Jan Kristinus <jan.kristinus@yakamara.de>
2017-06-19 16:25:02 +0200

    user_group bei un/install korrigiert

--- plugins/group/install.php
+++ plugins/group/install.php
@@ -1,14 +1,15 @@
 <?php
 
-// TODO:
-// rex_ycom_user table ergaenzen
-// gruppe hinzufügen
-
 rex_sql_table::get(rex::getTable('article'))
     ->ensureColumn(new rex_sql_column('ycom_group_type', "ENUM('0','1','2','3')", false, '0'))
     ->ensureColumn(new rex_sql_column('ycom_groups', 'varchar(255)'))
     ->alter()
 ;
 
-$content = rex_file::get(rex_path::plugin('ycom', 'group', 'install/tablesets/yform_group.json'));
+$content = rex_file::get(rex_path::plugin('ycom', 'group', 'install/tablesets/yform_ycom_group.json'));
 rex_yform_manager_table_api::importTablesets($content);
+
+$content = rex_file::get(rex_path::plugin('ycom', 'group', 'install/tablesets/yform_ycom_user_group.json'));
+rex_yform_manager_table_api::importTablesets($content);
+
+
--- plugins/group/uninstall.sql
+++ plugins/group/uninstall.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS `%TABLE_PREFIX%ycom_group`;
 DELETE FROM `%TABLE_PREFIX%yform_table` WHERE table_name = '%TABLE_PREFIX%ycom_group';
-DELETE FROM `%TABLE_PREFIX%yform_field` WHERE table_name = '%TABLE_PREFIX%ycom_group';
+DELETE FROM `%TABLE_PREFIX%yform_field` WHERE `table` = '%TABLE_PREFIX%ycom_group';
