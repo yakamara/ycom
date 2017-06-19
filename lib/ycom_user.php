f6d281049f5e2969e8009a254d8c4266949dde16
Jan Kristinus <jan.kristinus@yakamara.de>
2017-06-19 17:01:26 +0200

    isInGroup() fixed #96

--- lib/ycom_user.php
+++ lib/ycom_user.php
@@ -2,10 +2,24 @@
 
 class rex_ycom_user extends \rex_yform_manager_dataset
 {
-
-    static function getMe()
+    public static function getMe()
     {
         return rex_ycom_auth::getUser();
     }
 
+    public function isInGroup($group_id)
+    {
+        $ycom_groups = $this->getValue('ycom_groups');
+
+        if ($group_id == '') {
+            return true;
+        } elseif ($ycom_groups != '') {
+            $ycom_groups_array = explode(',', $ycom_groups);
+            if (in_array($group_id, $ycom_groups_array)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
