041aa7bd507fdc1942a074d0ef5f532c57134526
Jan Kristinus <jan.kristinus@yakamara.de>
2017-10-10 01:19:43 +0200

    Authplugin wird standardmässig nun mitinstalliert. Group Plugin standardmässig mit aktualisiert

--- install.php
+++ install.php
@@ -1,7 +1,10 @@
 <?php
 
-$content = rex_file::get(rex_path::addon('ycom','install/tablesets/yform_user.json'));
+$content = rex_file::get(rex_path::addon('ycom', 'install/tablesets/yform_user.json'));
 rex_yform_manager_table_api::importTablesets($content);
 
+$this->getPlugin('auth')->includeFile(__DIR__.'/plugins/auth/install.php');
 
-
+if ($this->getPlugin('group')->isInstalled()) {
+    $this->getPlugin('group')->includeFile(__DIR__.'/plugins/group/install.php');
+}
