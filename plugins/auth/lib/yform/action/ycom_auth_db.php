695c502b406116c45e9e1f900c15c179a644c381
Jan Kristinus <jan.kristinus@yakamara.de>
2017-11-01 12:45:09 +0100

    Extension Point bei YCOM Auth DB ergänzt #157

--- plugins/auth/lib/yform/action/ycom_auth_db.php
+++ plugins/auth/lib/yform/action/ycom_auth_db.php
@@ -5,17 +5,20 @@ class rex_yform_action_ycom_auth_db extends rex_yform_action_abstract
     public function execute()
     {
         $user = rex_ycom_auth::getUser();
+        $action = '';
         if (!rex::isBackend() && !$user) {
             echo 'error - access denied - user not logged in';
         } else {
             switch ($this->getElement(2)) {
                 case 'logout':
                     rex_ycom_auth::clearUserSession();
+                    $action = 'logout';
                     break;
 
                 case 'delete':
                     rex_ycom_auth::deleteUser($user->getValue('id'));
                     rex_ycom_auth::clearUserSession();
+                    $action = 'delete';
                     break;
 
                 case 'update':
@@ -31,9 +34,21 @@ class rex_yform_action_ycom_auth_db extends rex_yform_action_abstract
                     }
                     $sql->setWhere('id='.$user->getValue('id').'');
                     $sql->update();
+                    $action = 'update';
                     break;
             }
         }
+
+        rex_extension::registerPoint(new rex_extension_point('REX_YCOM_YFORM_SAVED', $sql,
+            [
+                'form' => $this,
+                'sql' => $sql,
+                'table' => rex_ycom_user::table(),
+                'action' => $action,
+                'id' => $this->params['main_id'],
+                'yform' => true,
+            ]
+        ));
     }
 
     public function getDescription()
