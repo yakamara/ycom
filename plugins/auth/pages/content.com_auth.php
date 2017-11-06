170388c341391fc0736dc36e3b669bee304c9f8f
Jan Kristinus <jan.kristinus@yakamara.de>
2017-11-06 12:48:10 +0100

    Seitenrechte auch anzeigen, wenn nicht veränderbar. fixed #160

--- plugins/auth/pages/content.com_auth.php
+++ plugins/auth/pages/content.com_auth.php
@@ -1,27 +1,5 @@
 <?php
 
-if (!rex::getUser()->hasPerm('ycomArticlePermissions[]')) {
-    $content = '';
-    $addon = rex_addon::get('ycom');
-
-    $article_id = $params['article_id'];
-    $clang = $params['clang'];
-    $ctype = $params['ctype'];
-
-    $yform = new rex_yform();
-    $yform->setObjectparams('submit_btn_show', false);
-
-    $yform->setValueField('select', ['ycom_auth_type', $addon->i18n('ycom_auth_perm'), rex_ycom_auth::$perms, '', 0]);
-    $yform = rex_extension::registerPoint(new rex_extension_point('YCOM_ARTICLE_PERM_SELECT', $yform, [
-        'article_id' => $article_id,
-    ]));
-
-    $form = $yform->getForm();
-    $form .= '<script>$( document ).ready(function() { $("#rex-page-sidebar-ycom_auth-perm :input").attr("disabled", true); }); </script>';
-    $form = '<section id="rex-page-sidebar-ycom_auth-perm" data-pjax-container="#rex-page-sidebar-ycom_auth-perm" data-pjax-no-history="1"><div><p class="alert alert-danger">'.$addon->i18n('no_permission_to_edit').'</p></div>'.$form.'</section>';
-    return $form;
-}
-
 $content = '';
 $addon = rex_addon::get('ycom');
 
@@ -47,16 +25,24 @@ $yform = rex_extension::registerPoint(new rex_extension_point('YCOM_ARTICLE_PERM
     'article_id' => $article_id,
 ]));
 
-$yform->setActionField('db', [rex::getTable('article'), 'id = ' . $article_id]);
-$yform->setObjectparams('submit_btn_label', $addon->i18n('ycom_auth_update_perm'));
+$permission_info = '';
+if (!rex::getUser()->hasPerm('ycomArticlePermissions[]')) {
+    $yform->setObjectparams('submit_btn_show', false);
+    $permission_info .= '<script>$( document ).ready(function() { $("#rex-page-sidebar-ycom_auth-perm :input").attr("disabled", true); }); </script>';
+    $permission_info .= '<div><p class="alert alert-danger">'.$addon->i18n('no_permission_to_edit').'</p></div>';
+} else {
+    $yform->setActionField('db', [rex::getTable('article'), 'id = ' . $article_id]);
+    $yform->setObjectparams('submit_btn_label', $addon->i18n('ycom_auth_update_perm'));
+}
+
 $form = $yform->getForm();
 
-if ($yform->objparams['actions_executed']) {
+if ($yform->objparams['actions_executed'] && rex::getUser()->hasPerm('ycomArticlePermissions[]')) {
     // TODO: trigger ARTICLE_UPDATE
     $form = rex_view::success($addon->i18n('ycom_auth_perm_updated')) . $form;
     rex_article_cache::delete($article_id, $clang);
 }
 
-$form = '<section id="rex-page-sidebar-ycom_auth-perm" data-pjax-container="#rex-page-sidebar-ycom_auth-perm" data-pjax-no-history="1">'.$form.'</section>';
+$form = '<section id="rex-page-sidebar-ycom_auth-perm" data-pjax-container="#rex-page-sidebar-ycom_auth-perm" data-pjax-no-history="1">'.$permission_info.$form.'</section>';
 
 return $form;
