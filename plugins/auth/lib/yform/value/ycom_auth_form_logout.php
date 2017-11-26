42ff7193ff7f84ea9d39c96930f794db88bfd8f0
Jan Kristinus <jan.kristinus@yakamara.de>
2017-11-26 02:36:43 +0100

    Logout korrigiert

--- plugins/auth/lib/yform/value/ycom_auth_form_logout.php
+++ plugins/auth/lib/yform/value/ycom_auth_form_logout.php
@@ -5,9 +5,9 @@ class rex_yform_value_ycom_auth_form_logout extends rex_yform_value_abstract
     public function enterObject()
     {
         if (!rex::isBackend()) {
-            ob_end_clean();
-            ob_end_clean();
-            header('Location:'.rex_getUrl(rex_config::get('ycom', 'article_logout'), '', [rex_config::get('ycom', 'auth_request_logout') => 1], '&'));
+            rex_response::cleanOutputBuffers();
+            rex_ycom_auth::clearUserSession();
+            header('Location:'.rex_getUrl(rex_config::get('ycom/auth', 'article_id_jump_logout'), '', [], '&'));
             exit;
         }
     }
