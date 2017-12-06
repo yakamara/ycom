4bcd6db19ca755c072d98b941c1c30bfc6e058dc
Tobias Krais <tk@design-to-use.de>
2017-12-06 11:05:26 +0100

    JS jetzt als eigenständige Datei

--- boot.php
+++ boot.php
@@ -23,41 +23,5 @@ rex_ycom::addTable('rex_ycom_user');
 rex_yform_manager_dataset::setModelClass('rex_ycom_user', rex_ycom_user::class);
 
 if(rex::isBackend() && rex_url::currentBackendPage() == "index.php?page=content/edit") {
-	rex_extension::register('OUTPUT_FILTER', 'append_ycom_script');
-}
-
-/**
- * Adds JS to media pool, showing goup field only if needed
- * @param rex_extension_point $ep Redaxo extension point
- */
-function append_ycom_script(rex_extension_point $ep) {
-	// Insert before </body> element
-	$insert_body = "<script type='text/javascript'>". PHP_EOL
-		."function change_ycom_fields() {". PHP_EOL
-		."	if($('#yform-ycom_auth-perm-field-0 option:selected').val() != 1) {". PHP_EOL
-		."		$('#yform-ycom_auth-perm-field-1').closest('.form-group').hide();". PHP_EOL
-		."		$('#yform-ycom_auth-perm-field-2').closest('.form-group').hide();". PHP_EOL
-		."	}". PHP_EOL
-		."	else {". PHP_EOL
-		."		$('#yform-ycom_auth-perm-field-1').closest('.form-group').show();". PHP_EOL
-		."		if($('#yform-ycom_auth-perm-field-1 option:selected').val() == 1 || $('#yform-ycom_auth-perm-field-1 option:selected').val() == 2) {". PHP_EOL
-		."			$('#yform-ycom_auth-perm-field-2').closest('.form-group').show();". PHP_EOL
-		."		}". PHP_EOL
-		."		else {". PHP_EOL
-		."			$('#yform-ycom_auth-perm-field-2').closest('.form-group').hide();". PHP_EOL
-		."		}". PHP_EOL
-		."	}". PHP_EOL
-		."}". PHP_EOL
-
-		."$(document).on('rex:ready', function () {". PHP_EOL
-		."	change_ycom_fields();". PHP_EOL	
-		."	$('#yform-ycom_auth-perm-field-0').on('change', function() {". PHP_EOL
-		."		change_ycom_fields();". PHP_EOL
-		."	});". PHP_EOL
-		."	$('#yform-ycom_auth-perm-field-1').on('change', function() {". PHP_EOL
-		."		change_ycom_fields();". PHP_EOL
-		."	});". PHP_EOL
-		."});". PHP_EOL
-		."</script>";
-	$ep->setSubject(str_replace('</body>', $insert_body .'</body>', $ep->getSubject()));
+	rex_view::addJsFile($this->getAssetsUrl('ycom_backend.js'));
 }
