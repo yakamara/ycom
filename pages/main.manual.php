6332c48da8f1543bb3d9f19685f7ec036be4a02f
Wolfgang Bund <wb@dtp-net.de>
2018-01-10 09:10:39 +0100

    Doku ergänzt. PW Rücksetzen Funktion, csrf Protection, E-Mail Templates

--- pages/main.manual.php
+++ pages/main.manual.php
@@ -13,8 +13,11 @@ $file_form_register = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_f
 $file_form_register_proof = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_register_proof.inc'));
 $file_form_profile = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_profile.inc'));
 $file_form_change_password = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_change_password.inc'));
+$file_form_reset_password = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_reset_password.inc'));
+$file_form_reset_password_proof = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_reset_password_proof.inc'));
 
 $file_emailtemplate_registration = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_emailtemplate_registration.inc'));
+$file_emailtemplate_resetpassword = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_emailtemplate_resetpassword.inc'));
 
 $content .= rex_ycom::parseText($file_allgemein_txt);
 $content .= '<hr/>';
@@ -34,9 +37,16 @@ $content .= '<br/>Profil:<br/>';
 $content .= rex_string::highlight($file_form_profile);
 $content .= '<br/>Password ändern:<br/>';
 $content .= rex_string::highlight($file_form_change_password);
-$content .= '<h3>Bestätigungs E-Mail</h3>';
-$content .= '<br/>Beispiel für das yform Template <code>access_request_de</code> für die Bestätigungsmail (HTML):<br/>';
+$content .= '<br/>Password zurücksetzen:<br/>';
+$content .= rex_string::highlight($file_form_reset_password);
+$content .= '<br/>Password zurücksetzen Bestätigung:<br/>';
+$content .= rex_string::highlight($file_form_reset_password_proof);
+$content .= '<h3>E-Mail Templates</h3>';
+$content .= '<code>access_request_de</code> für die Bestätigungsmail (HTML):<br/>';
 $content .= rex_string::highlight($file_emailtemplate_registration);
+$content .= '<code>resetpassword_de</code> für die Passwort zurücksetzen Mail (HTML):<br/>';
+$content .= rex_string::highlight($file_emailtemplate_resetpassword);
+
 
 $fragment = new rex_fragment();
 $fragment->setVar('title', $this->i18n('ycom_manual_title'), '');
