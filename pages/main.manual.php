ccc0e5ccce9dc269372f2799b34f28bcfd7d73f0
Wolfgang Bund <wb@dtp-net.de>
2018-01-10 07:54:45 +0100

    Neu: Beispiel E-Mail Template

--- pages/main.manual.php
+++ pages/main.manual.php
@@ -9,11 +9,12 @@ $file_userheader_navi_txt = rex_file::get(rex_path::addon('ycom', 'pages/main.ma
 $file_userheader_navi_code = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_user_header_navi_code.inc'));
 
 $file_form_login = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_login.inc'));
-$file_form_logout = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_logout.inc'));
 $file_form_register = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_register.inc'));
 $file_form_register_proof = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_register_proof.inc'));
 $file_form_profile = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_profile.inc'));
 $file_form_change_password = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_form_change_password.inc'));
+
+$file_emailtemplate_registration = rex_file::get(rex_path::addon('ycom', 'pages/main.manual_emailtemplate_registration.inc'));
 
 $content .= rex_ycom::parseText($file_allgemein_txt);
 $content .= '<hr/>';
@@ -25,8 +26,6 @@ $content .= '<hr/>';
 $content .= '<h3>Formulare</h3>';
 $content .= '<br/>Login:<br/>';
 $content .= rex_string::highlight($file_form_login);
-$content .= '<br/>Logout:<br/>';
-$content .= rex_string::highlight($file_form_logout);
 $content .= '<br/>Registrierung:<br/>';
 $content .= rex_string::highlight($file_form_register);
 $content .= '<br/>Registrierungsbestätigung:<br/>';
@@ -35,6 +34,9 @@ $content .= '<br/>Profil:<br/>';
 $content .= rex_string::highlight($file_form_profile);
 $content .= '<br/>Password ändern:<br/>';
 $content .= rex_string::highlight($file_form_change_password);
+$content .= '<h3>Bestätigungs E-Mail</h3>';
+$content .= '<br/>Beispiel für das yform Template <code>access_request_de</code> für die Bestätigungsmail (HTML):<br/>';
+$content .= rex_string::highlight($file_emailtemplate_registration);
 
 $fragment = new rex_fragment();
 $fragment->setVar('title', $this->i18n('ycom_manual_title'), '');
