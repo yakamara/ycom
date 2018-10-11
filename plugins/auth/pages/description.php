7038d22a0b1e5a5128cb44683595423ae563fa7b
Jan Kristinus <jan.kristinus@yakamara.de>
2018-10-11 10:46:44 +0200

    unnötige datei entfernt

--- plugins/auth/pages/description.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$content = 'Folgt...<br/>';
-
-$fragment = new rex_fragment();
-$fragment->setVar('title', $this->i18n('ycom_auth_info_title'));
-$fragment->setVar('body', $content, false);
-echo $fragment->parse('core/page/section.php');
