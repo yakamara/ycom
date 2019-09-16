c769e034e103bd48d0a170957e448e2455edba7e
Jan Kristinus <jan.kristinus@yakamara.de>
2019-09-16 17:42:22 +0200

    auth: validate_ycom_auth. Warnings werden nun ans Objekt übergeben #224

--- plugins/auth/lib/yform/validate/ycom_auth.php
+++ plugins/auth/lib/yform/validate/ycom_auth.php
@@ -2,27 +2,37 @@
 
 class rex_yform_validate_ycom_auth extends rex_yform_validate_abstract
 {
-    public function postValueAction()
+    public function enterObject()
     {
-        $login = '';
-        $psw = '';
-        $stay = true;
 
-        foreach ($this->params['value_pool']['sql'] as $k => $v) {
-            if ($k == $this->getElement(2)) {
-                $login = $v;
-            } elseif ($k == $this->getElement(3)) {
-                $psw = $v;
-            } elseif ($k == $this->getElement(4)) {
-                if (1 == $v) {
-                    $stay = true;
+        $loginObject = null;
+        $passwordObject = null;
+        $stayObject = null;
+
+        $warningObjects = [];
+        foreach ($this->getObjects() as $Object) {
+            if ($this->isObject($Object)) {
+                if ($Object->getName() == $this->getElement(2)) {
+                    $loginObject = $Object;
+                    $warningObjects[] = $Object;
+                }
+                if ($Object->getName() == $this->getElement(3)) {
+                    $passwordObject = $Object;
+                    $warningObjects[] = $Object;
+                }
+                if ($Object->getName() == $this->getElement(4)) {
+                    $stayObject = $Object;
                 }
             }
         }
 
-        if ('' == $login || '' == $psw) {
-            $this->params['warning'][] = 1;
-            $this->params['warning_messages'][] = $this->getElement(5);
+        if ('' == $loginObject->getValue() || '' == $passwordObject->getValue()) {
+
+            foreach($warningObjects as $warningObject) {
+                dump($warningObject);
+                $this->params['warning'][$warningObject->getId()] = $this->params['error_class'];
+                $this->params['warning_messages'][$warningObject->getId()] = $this->getElement(5);
+            }
             rex_ycom_auth::clearUserSession();
             return;
         }
@@ -37,15 +47,17 @@ class rex_yform_validate_ycom_auth extends rex_yform_validate_abstract
         */
 
         $params = [];
-        $params['loginName'] = $login;
-        $params['loginPassword'] = $psw;
-        $params['loginStay'] = $stay;
+        $params['loginName'] = ($loginObject) ? $loginObject->getValue(): '';
+        $params['loginPassword'] = ($passwordObject) ? $passwordObject->getValue(): '';
+        $params['loginStay'] = ($stayObject) ? $stayObject->getValue() : false;
 
         $status = rex_ycom_auth::login($params);
 
         if (2 != $status) {
-            $this->params['warning'][] = 1;
-            $this->params['warning_messages'][] = $this->getElement(6);
+            foreach($warningObjects as $warningObject) {
+                $this->params['warning'][$warningObject->getId()] = $this->params['error_class'];
+                $this->params['warning_messages'][$warningObject->getId()] = $this->getElement(6);
+            }
             rex_ycom_auth::clearUserSession();
         }
     }
