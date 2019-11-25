88ebc1d8f7b1b04a1265f940d834df86c5a474d8
Jan Kristinus <jan.kristinus@yakamara.de>
2019-11-25 13:30:38 +0100

    auth: install - debug entfernt.

--- plugins/auth/install.php
+++ plugins/auth/install.php
@@ -15,7 +15,6 @@ rex_file::copy(__DIR__.'/install/saml.php', rex_addon::get('ycom')->getDataPath(
 // termofuse -> termsofuse. Version < 3.0
 try {
     rex_sql::factory()
-    ->setDebug()
     ->setQuery('delete from `' . rex_yform_manager_field::table() . '` where `table_name`="rex_ycom_user" and `type_id`="value" and `type_name`="checkbox" and `name`="termofuse_accepted"', [])
     ->setQuery('update `rex_config` set `key`="article_id_jump_termsofuse" where `key`="article_id_jump_termofuse" and `namespace`="ycom/auth"', [])
     ->setQuery('alter table `rex_ycom_user` drop if exists `termofuse_accepted`', [])
