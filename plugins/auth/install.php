633de43210b21622049dd68b7c83c3111a7fbe20
Jan Kristinus <jan.kristinus@yakamara.de>
2020-07-03 08:14:01 +0200

    Aufruf von rex_ycom_user Tabelle optimiert

--- plugins/auth/install.php
+++ plugins/auth/install.php
@@ -19,7 +19,7 @@ try {
     rex_sql::factory()
     ->setQuery('delete from `' . rex_yform_manager_field::table() . '` where `table_name`="rex_ycom_user" and `type_id`="value" and `type_name`="checkbox" and `name`="termofuse_accepted"', [])
     ->setQuery('update `rex_config` set `key`="article_id_jump_termsofuse" where `key`="article_id_jump_termofuse" and `namespace`="ycom/auth"', [])
-    ->setQuery('alter table `rex_ycom_user` drop if exists `termofuse_accepted`', [])
+    ->setQuery('alter table `' . rex_ycom_user::table() . '` drop if exists `termofuse_accepted`', [])
     ;
 } catch (rex_sql_exception $e) {
     // dump($e);
