49e24bd1dce319350f251c14d6aef23da3a1b027
Jan Kristinus <jan.kristinus@yakamara.de>
2020-07-17 09:46:56 +0200

    auth: install mit termsofuse_accepted verbessert

--- plugins/auth/install.php
+++ plugins/auth/install.php
@@ -16,13 +16,20 @@ rex_file::copy(__DIR__.'/install/cas.php', rex_addon::get('ycom')->getDataPath('
 
 // termofuse -> termsofuse. Version < 3.0
 try {
+
+    rex_sql_table::get(rex::getTablePrefix().'ycom_user')
+        ->ensureColumn(new rex_sql_column('termsofuse_accepted', 'tinyint(1)', false, '0'))
+        ->alter();
+
     rex_sql::factory()
-    ->setQuery('delete from `' . rex_yform_manager_field::table() . '` where `table_name`="rex_ycom_user" and `type_id`="value" and `type_name`="checkbox" and `name`="termofuse_accepted"', [])
-    ->setQuery('update `rex_config` set `key`="article_id_jump_termsofuse" where `key`="article_id_jump_termofuse" and `namespace`="ycom/auth"', [])
-    ->setQuery('alter table `' . rex_ycom_user::table() . '` drop if exists `termofuse_accepted`', [])
+        ->setQuery('alter table `' . rex::getTablePrefix().'ycom_user' . '` drop if exists `termofuse_accepted`', [])
+        ->setQuery('delete from `' . rex_yform_manager_field::table() . '` where `table_name`="rex_ycom_user" and `type_id`="value" and `type_name`="checkbox" and `name`="termofuse_accepted"', [])
+        ->setQuery('update `rex_config` set `key`="article_id_jump_termsofuse" where `key`="article_id_jump_termofuse" and `namespace`="ycom/auth"', [])
     ;
+
 } catch (rex_sql_exception $e) {
-    // dump($e);
+    dump($e);
+    exit;
 }
 
 rex_delete_cache();
