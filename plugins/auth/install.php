e5a34b4df202ae182ae6f6cb61256ade8390d2ee
Jan Kristinus <jan.kristinus@yakamara.de>
2020-07-17 11:03:04 +0200

    termofuse_accepted wird nicht mehr aus Tabelle gelöscht

--- plugins/auth/install.php
+++ plugins/auth/install.php
@@ -22,7 +22,7 @@ try {
         ->alter();
 
     rex_sql::factory()
-        ->setQuery('alter table `' . rex::getTablePrefix().'ycom_user' . '` drop if exists `termofuse_accepted`', [])
+//        ->setQuery('alter table `' . rex::getTablePrefix().'ycom_user' . '` drop if exists `termofuse_accepted`', [])
         ->setQuery('delete from `' . rex_yform_manager_field::table() . '` where `table_name`="rex_ycom_user" and `type_id`="value" and `type_name`="checkbox" and `name`="termofuse_accepted"', [])
         ->setQuery('update `rex_config` set `key`="article_id_jump_termsofuse" where `key`="article_id_jump_termofuse" and `namespace`="ycom/auth"', [])
     ;
