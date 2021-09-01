b88b46a332213df9b50eba77916f915ce4161cdb
Jan Kristinus <jan.kristinus@yakamara.de>
2021-09-01 14:10:56 +0200

    groups: beim unsinstall werden nun auch die ycom_groups in der usertabelle gelöscht #346

--- plugins/group/uninstall.php
+++ plugins/group/uninstall.php
@@ -3,6 +3,7 @@
 $sql = rex_sql::factory();
 $sql->setQuery('DELETE FROM `'.rex::getTable('yform_table').'` WHERE table_name = "'.rex::getTable('ycom_group').'"');
 $sql->setQuery('DELETE FROM `'.rex::getTable('yform_field').'` WHERE table_name = "'.rex::getTable('ycom_group').'"');
+$sql->setQuery('DELETE FROM `'.rex::getTable('yform_field').'` WHERE table_name = "'.rex::getTable('ycom_user').'" and name="ycom_groups"');
 $sql->setQuery('DELETE FROM `'.rex::getTable('yform_history').'` WHERE table_name = "'.rex::getTable('ycom_group').'"');
 
 rex_sql_table::get(rex::getTable('ycom_group'))
