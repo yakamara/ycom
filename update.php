c1b51654da3d11f70cf1d3ae9da087880f318f3c
Jan Kristinus <jan.kristinus@yakamara.de>
2022-11-11 11:04:08 +0100

    Update/Install wurden doppelt ausgeführt

--- update.php
+++ update.php
@@ -6,12 +6,3 @@
  */
 
 $this->includeFile(__DIR__.'/install.php');
-
-foreach ($this->getInstalledPlugins() as $plugin) {
-    // use path relative to __DIR__ to get correct path in update temp dir
-    $file = __DIR__ . '/plugins/' . $plugin->getName() . '/update.php';
-
-    if (file_exists($file)) {
-        $plugin->includeFile($file);
-    }
-}
